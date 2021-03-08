787b71090f32b8f8f3e1a34d4497594db0f3fbe1
maxscha <maxscha@mail.de>
2021-03-09 00:18:01 +0100

    Init with hacked together prototype

---
+++ public/javascripts/index.js
@@ -0,0 +1,48 @@
+// var videojs = require('video.js')
+
+function wrongAnswer() {
+    document.getElementById('answer-div').innerText = "wrong";
+}
+
+function rightAnswer() {
+    document.getElementById('answer-div').innerText = "right!";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // code...
+
+    var player = videojs('my-video');
+
+    var ModalDialog = videojs.getComponent('ModalDialog');
+
+    // var modal = new ModalDialog(player, {
+
+    // We don't want this modal to go away when it closes.
+    // temporary: false
+    // });
+    // 
+    // modal.content("test")
+
+    var btn = document.createElement("div");   // Create a <button> element
+    btn.innerHTML = `
+    <div>
+        <span class="question">What will you learn in this video?</span>
+    </div>
+    <button onclick="wrongAnswer()"> Nothing </button> 
+    <button onclick="rightAnswer()"> As much python as possible </button>
+    <div id="answer-div">
+    </div>
+    `;
+
+    // On click
+
+    var wasOpen = false;
+    var pausetime = 5;
+    player.on('timeupdate', function (e) {
+        if (player.currentTime() >= pausetime && !wasOpen) {
+            var modal = player.createModal(btn);
+            wasOpen = true;
+        }
+    });
+
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# VideoJS Question Plugin
+
+The aim is to recreate the question plugin from coursera for video.js. This would make it possible to enhance videos
